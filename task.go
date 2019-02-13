f2deedc7e2624e6a62ae4482277f39531b51b6ad
Timidger <APragmaticPlace@gmail.com>
2019-02-12 23:41:19 -0500

    Added basic task that I'm pretty sure about.
    Like I need some body, and I need a due date. Though that last one might
    be optional.

---
+++ task.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type Task struct {
+	// The "body" content of the task, in Markdown
+	body_content string
+	// When this task is due and must be done.
+	// What this actually means is still up for debate.
+	// However, I feel strongly that this should "explode" in some way.
+	due_date time.Time
+}
